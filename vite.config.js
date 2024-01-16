02fa67da2fdf705bf572ed51e2ab70491b68ac95
unknown <poloj3614@gmail.com>
2024-01-15 21:13:22 -0500

    base agregada

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://JuanJosePl.github.io/vite-deploy' ,
   css: {
     modules: {
       localsConvention: "camelCase",
