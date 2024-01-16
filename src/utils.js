bed3b200ab9e86116e959d52ee5b4e2a3065c467
unknown <poloj3614@gmail.com>
2024-01-16 12:41:03 -0500

    La carpeta dist fue eliminada

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export const getImageUrl = (path) => {
     return new URL(`/assets/${path}`, import.meta.url).href;
 };
+
+export const getFileUrl = (path) => {
+    return new URL(`/assets/${path}`, import.meta.url).href;
+};
